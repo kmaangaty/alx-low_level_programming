56f65debce0c750c66370ed2d6b07dc7546c233f
kmaangaty <kmaangaty@gmail.com>
2023-02-17 14:47:42 -0800

    fix 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,8 +6,10 @@
  */
 #include <stdio.h>
 
-/*
- * main - entry point
+/**
+ * main - program that prints the alphabet in
+ *  lowercase, followed by a new line.
+ * Return: 0 for the main function.
  */
 
 int main(void)
